13d959f954666b993a82c465d869bddc06a45be7
IanGu1019 <yanyaogu@gmail.com>
2024-11-28 16:12:59 -0500

    added 9 functions for Poisson distribution

--- R/Poisson.R
+++ R/Poisson.R
@@ -1,5 +1,63 @@
-# MLE functions for Poisson distriubtion
+# MLE functions for Poisson distribution
 
-pll <- function(lambda, n, x){
+# log likelihood function
+poisll <- function(lambda, x){
+  if(lambda<=0){stop("Lambda must be greater than 0.")}
+  if(any(x<0)){stop("There is at least one negative value in the dataset.")}
+  n <- length(x)
+  sum_x <- sum(x)
+  sum_x * log(lambda) - n * lambda - sum(lfactorial(x))
+}
 
+# Score function
+poisS <- function(lambda, x){
+  if(lambda<=0){stop("Lambda must be greater than 0.")}
+  if(any(x<0)){stop("There is at least one negative value in the dataset.")}
+  sum_x <- sum(x)
+  n <- length(x)
+  sum_x / lambda - n
+}
+
+# Information function
+poisI <- function(lambda, x){
+  if(lambda<=0){stop("Lambda must be greater than 0.")}
+  if(any(x<0)){stop("There is at least one negative value in the dataset.")}
+  n <- length(x)
+  n / lambda
+}
+
+# relative log likelihood function
+poisrll <- function(lambda, lambdahat, x){
+  if(lambda<=0){stop("Lambda must be greater than 0.")}
+  if(lambdahat<=0){stop("Lambdahat must be greater than 0.")}
+  if(any(x<0)){stop("There is at least one negative value in the dataset.")}
+  poisll(lambda,x)-poisll(lambdahat,x)
+}
+
+# function for likelihood intervals
+poisLI <- function(lambdahat, x, p){
+  if(lambdahat<=0){stop("Lambdahat must be greater than 0.")}
+  if(any(x<0)){stop("There is at least one negative value in the dataset.")}
+  poisLIc <- function(lambda, lambdahat, x, p){
+    poisrll(lambda,lambdahat,x)-log(p)
+  }
+  lower <- uniroot(poisLIc,c(10^(-8),lambdahat),lambdahat=lambdahat,x=x,p=p)$root
+  upper <- uniroot(poisLIc,c(lambdahat,lambdahat*10^8),lambdahat=lambdahat,x=x,p=p)$root
+  return(c(lower,upper))
+}
+
+# likelihood ratio statistic
+poisD <- function(lambda, lambdahat, x){
+  if(lambda<=0){stop("Lambda must be greater than 0.")}
+  if(lambdahat<=0){stop("Lambdahat must be greater than 0.")}
+  if(any(x<0)){stop("There is at least one negative value in the dataset.")}
+  -2*poisrll(lambda,lambdahat,x)
+}
+
+# confidence interval based on likelihood ratio statistic
+poisDci <- function(lambda, lambdahat, x, conf) {
+  if(lambda<=0){stop("Lambda must be greater than 0.")}
+  if(lambdahat<=0){stop("Lambdahat must be greater than 0.")}
+  if(any(x<0)){stop("There is at least one negative value in the dataset.")}
+  -2 * poisrll(lambda, lambdahat, x) -qchisq(conf, 1)
 }
