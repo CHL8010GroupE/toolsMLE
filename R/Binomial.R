24d306a617dca298eb32b01ad03e8f5068f378e2
mugengliu <mugeng.liu@mail.utoronto.ca>
2024-12-01 03:20:05 +0800

    Update the MLE for binomial distribution

--- R/Binomial.R
+++ R/Binomial.R
@@ -1,7 +1,17 @@
 # MLE for Binomial Distribution
 # example of data: c(1,2,4,6,2,2,1,5,6,2,4,5,3), a list of vector that record success for each trail
 
-# binomial likelihood function
+#binomial likelihood function
+MLE.binol <- function(p, data, trials){
+  if (p < 0 || p > 1) {
+    stop("p must be within (0, 1)")
+  }
+  prod(p^(data) * (1 - p)^(trials - data) * factorial(trials) / (factorial(data) * factorial(trials - data)))
+}
+
+
+
+# binomial log likelihood function
 MLE.binoll <- function(p, data, trials){
   if (p < 0 || p > 1) {
     stop("p must be within (0, 1)")
@@ -17,17 +27,45 @@ MLE.binomial <- function(data, trials) {
   optimize(MLE.binoll, c(1e-10, 1 - 1e-10), data = data, trials = trials, maximum = TRUE)
 }
 
+# Score Function
+MLE.binomialS <-function(p, data, trials){
+  sum(data / p - (trials - data) / (1 - p))
+}
+
+# Information function
+MLE.binomialI <- function(p, data, trials){
+  -sum((trials * p  / p^2) + ((trials) * (1 - p) / (1 - p)^2))
+}
+
+# Relative Likelihood Function
+MLE.binomialrll <- function(p, phat, data, trials){
+  MLE.binoll(p, data, trials) - MLE.binoll(phat, data, trials)
+
+}
+
+# Function for likelihood intervals
+MLE.binomialLI <- function(data, trials, alpha){
+  estimated <- MLE.binomial(data, trials)$maximum
+  MLE.binomialLIc <- function(data, trials, p, phat, alpha){
+    MLE.binomialrll(p, phat, data, trials) - log(1 - alpha)
+  }
+  lower <- uniroot(MLE.binomialLIc, interval = c(1e-10, estimated), phat = estimated, data = data, trials = trials, alpha = alpha)$root
+
+  upper <- uniroot(MLE.binomialLIc, interval = c(estimated, 1 - 1e-10), phat = estimated, data = data, trials = trials, alpha = alpha)$root
+
+  c(lower = lower, upper = upper)
+}
 
 # Likelihood Ratio Test
-MLE.binomialD <- function(data, trails, p_test){
+MLE.binomialD <- function(data, trials, p_test){
   if (p_test < 0 || p_test > 1){
     stop("p-test must be within (0, 1)")
   }
   # generate log likelihood for null, p_test
-  log_test <- MLE.binoll(p_test, data, trails)
+  log_test <- MLE.binoll(p_test, data, trials)
 
   # generate log likelihood for p_alter
-  estimated_parameter <- MLE.binomial(data, trails)
+  estimated_parameter <- MLE.binomial(data, trials)
 
 
   log_alter <- estimated_parameter$objective
@@ -43,8 +81,13 @@ MLE.binomialD <- function(data, trails, p_test){
 }
 
 
+# confidence interval based on likelihood ratio statistic
+MLE.binomialDci <- function(ptest, data, trials, conf){
+  # To do
+}
+
 # Goodness of fit test: test whether data follows a binomial distribution, print p-value
-MLE.binomialGOF <- function(data, trails){
+MLE.binomialGOF <- function(data, trials){
   # Check for valid input
   if (any(data > trials)) {
     stop("Data values cannot exceed the number of trials.")
